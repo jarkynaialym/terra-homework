63c91344f8d6e4d81651255e349d6f2133f87b12
jarkynaialym <zzharkynai.8888@gmail.com>
2024-04-09 21:49:07 +0000

    homework done

--- homework3/tera.tf
+++ homework3/tera.tf
@@ -2,6 +2,11 @@ provider aws {
     region = "us-east-2"
 }
 
+variable "availability_zones" {
+  default = ["us-east-2a", "us-east-2b", "us-east-2c",]
+}
+
+
 data "aws_ami" "amzn2" {
   most_recent = true
 
@@ -18,59 +23,33 @@ data "aws_ami" "amzn2" {
   owners = ["137112412989"] 
 }
 
-
-
-
-
-
-
-
-
-
-
 resource "aws_instance" "web" {
-  ami = data.aws_ami.amzn2.id
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.amzn2.id
+  instance_type = "t2.micro"      
+  count         = length(var.availability_zones) 
+  availability_zone = var.availability_zones[count.index]
+  vpc_security_group_ids = [aws_security_group.of_the_ec2.id]
+  associate_public_ip_address = true 
+  key_name = aws_key_pair.deployer.key_name  
+  user_data = file("apache.sh") 
+  user_data_replace_on_change = true
+  
 
-    availability_zone = "us-east-2a"
-    vpc_security_group_ids = [aws_security_group.of_the_ec2.id]
-    key_name = aws_key_pair.deployer.key_name
-    user_data_replace_on_change = true
-    user_data = file("apache.sh")
-
-    tags = {
-    Name = "web-1"
-  }
-
-}
-
-resource "aws_instance" "web1" {
-  ami = data.aws_ami.amzn2.id
-  instance_type = "t2.micro"
-
-    availability_zone = "us-east-2b"
-    vpc_security_group_ids = [aws_security_group.of_the_ec2.id]
-    key_name = aws_key_pair.deployer.key_name
-    user_data_replace_on_change = true
-    user_data = file("apache.sh")
-
-tags = {
-    Name = "web-2"
+  tags = {
+    Name = "web-${count.index + 1}"  
   }
 }
 
-resource "aws_instance" "web2" {
-  ami = data.aws_ami.amzn2.id
-  instance_type = "t2.micro"
 
-    availability_zone = "us-east-2c"
-    vpc_security_group_ids = [aws_security_group.of_the_ec2.id]
-    key_name = aws_key_pair.deployer.key_name
-    user_data_replace_on_change = true
-    user_data = file("apache.sh")
 
-    tags = {
-    Name = "web-2"
-  }
 
-}
+
+
+
+
+
+
+
+
+
+
